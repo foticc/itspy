ce30737a8b985874272899f3eea1cd6dd7cfda87
foticc <foticc@163.com>
2017-08-09 16:50:52 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,17 @@
+#encoding:utf-8
+import requests
+from bs4 import BeautifulSoup
+import json
+import sys
+import re
+reload(sys)
+sys.setdefaultencoding("utf8")
+
+#url = 'https://github.com/timeline.json'
+url = 'http://www.lrcgc.com/lyric-210-301333.html'
+response = requests.get(url)
+soup = BeautifulSoup(response.text,"lxml")
+lrc = soup.find_all(id="J_lyric")
+lrc = lrc[0].get_text()
+lrc = re.sub('\[\d*\:\d*\.\d*\]', ',', lrc, count=0, flags=0)
+print lrc
