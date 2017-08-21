0cee340fa9f543fc64387db34de45657990045bd
foticc <foticc@163.com>
2017-08-21 18:07:21 +0800

    Add files via upload

--- 3.py
+++ 3.py
@@ -1,25 +1,52 @@
 import requests
 from bs4 import BeautifulSoup
+import xlwt
 import time
 
-
-url = 'http://www.lrcgc.com'
+path = 'C:\\Users\\rs\\Desktop\\text.xls'
+urlstr = 'http://www.lrcgc.com'
 home = '/artist-00.html'
+wb = xlwt.Workbook()
+sheet = wb.add_sheet('sheet-list',cell_overwrite_ok=True)
 
-#home'div.artist_category dl.current dd p a'
+
 def spider(url,select):
 	response = requests.get(url)
-	print dir(response)
-	print response.status_code
 	soup = BeautifulSoup(response.text,'lxml')
 	context = soup.select(select)
-	print context
 	return context
 
-
-def get_singer_list(url,select):
-	for x in spider(url, select):
-		print x.text
+# 'div.artist_category dl.current dd p a' length=9
+def get_singer_list(l,select):
+	hcontext = spider(l,select)
+	i=0
+	print len(hcontext),
+	for x in hcontext:
+		i+=1
+		sheet.write(i,0,x.text)
+		sheet.write(i,1,x['href'])
+		try:
+			if x['href']:
+				get_singer('http://www.lrcgc.com/'+x['href'],'div.p5 div.namelist ul.cc li a',i)
+		except Exception as e:
+			continue
 		
-if __name__ == '__main__':
-	get_singer_list(url+home,'.current dd p a')
+
+#'div.p5 div.namelist ul.cc li a'
+def get_singer(l,select,i):
+	for x in spider(l,select):
+		sheet.write(i,3,x.text)
+		sheet.write(i,4,x['href'])
+		if x['href']:
+			get_main_sing_list('http://www.lrcgc.com/'+x['href'],'div.thread_posts_list table tbody tr td a')
+
+
+
+#'div.thread_posts_list table tbody tr td a'
+def get_main_sing_list(l,select):
+	for x in spider(l,select):
+		print x.text,x['href']
+			
+		
+get_singer_list(urlstr+home,'div.artist_category dl.current dd p a')
+wb.save(path)
